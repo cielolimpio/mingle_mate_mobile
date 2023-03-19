9983b7de72eeffad844ce36f4f0997d212979461
dongsuu <suudh99@naver.com>
2023-03-19 16:02:05 +0900

    fix : button component

--- lib/component/elevated_button.dart
+++ lib/component/elevated_button.dart
@@ -4,14 +4,34 @@ class MingleMateElevatedButton extends StatelessWidget {
   final String text;
   final Color buttonColor;
   final Color textColor;
-  const MingleMateElevatedButton({required this.text, required this.buttonColor, required this.textColor, Key? key}) : super(key: key);
+  double? buttonWidth = 160.0;
+  double? buttonHeight = 60.0;
+
+  MingleMateElevatedButton(
+      {required this.text,
+      required this.buttonColor,
+      required this.textColor,
+      double? buttonWidth,
+      double? buttonHeight,
+      Key? key})
+      : super(key: key) {
+    if (buttonWidth != null) {
+      this.buttonWidth = buttonWidth;
+    }
+    if (buttonHeight != null) {
+      this.buttonHeight = buttonHeight;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (buttonWidth != null) buttonWidth = buttonWidth;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ElevatedButton(
-        style: ElevatedButton.styleFrom(backgroundColor: buttonColor),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: buttonColor,
+            fixedSize: Size(buttonWidth!, buttonHeight!)),
         onPressed: () {},
         child: Text(
           text,
